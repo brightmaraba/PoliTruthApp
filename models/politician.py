07964efedf55725d2d9ad60e4c74141bfc7d08eb
Brian Koech <brightkoech@gmail.com>
2020-02-29 00:09:26 +0800

    Restarted Construction to sort errors in deleting data

--- models/politician.py
+++ models/politician.py
@@ -6,24 +6,24 @@ def get_last_id():
         last_politician = politician_list[-1]
     else:
         return 1
-
     return last_politician.id + 1
 
 
 class Politician:
-    def __init__(self, name, position, description, gender, age, county,
-                    constituency, ward, bio_data, c_vitae):
+
+    def __init__(self, name, position, description, age, gender,
+                bio_data, c_vitae, county, constituency, ward):
         self.id = get_last_id()
         self.name = name
         self.position = position
         self.description = description
-        self.gender = gender
         self.age = age
+        self.gender = gender
+        self.bio_data = bio_data
+        self.c_vitae = c_vitae
         self.county = county
         self.constituency = constituency
         self.ward = ward
-        self.bio_data = bio_data
-        self.c_vitae = c_vitae
         self.is_publish = False
 
     @property
@@ -33,11 +33,11 @@ class Politician:
             'name': self.name,
             'position': self.position,
             'description': self.description,
-            'gender': self.gender,
             'age': self.age,
+            'gender': self.gender,
+            'bio_data': self.bio_data,
+            'c_vitae': self.c_vitae,
             'county': self.county,
             'constituency': self.constituency,
-            'ward': self.ward,
-            'bio_data': self.bio_data,
-            'c_vitae': self.c_vitae
+            'ward': self.ward
         }
--- resources/politician.py
+++ resources/politician.py
@@ -6,7 +6,9 @@ from models.politician import Politician, politician_list
 
 
 class PoliticianListResource(Resource):
+
     def get(self):
+
         data = []
 
         for politician in politician_list:
@@ -18,26 +20,19 @@ class PoliticianListResource(Resource):
     def post(self):
         data = request.get_json()
 
-        politician = Politician(
-            name=data['name'],
-            position=data['position'],
-            description=data['description'],
-            gender=data['gender'],
-            age=data['age'],
-            county=data['county'],
-            constituency=data['constituency'],
-            ward=data['ward'],
-            bio_data=data['bio_data'],
-            c_vitae=data['c_vitae']
+        politician = Politician(name=data['name'],
+                                description=data['description']
         )
+
         politician_list.append(politician)
+
         return politician.data, HTTPStatus.CREATED
 
 
 class PoliticianResource(Resource):
+
     def get(self, politician_id):
-        politician = next((politician for politician in politician_list
-                           if politician_id == politician_id and politician.is_publish == True), None)
+        politician = next((politician for politician in politician_list if politician.id == politician_id and politician.is_publish == True), None)
 
         if politician is None:
             return {'message': 'politician not found'}, HTTPStatus.NOT_FOUND
@@ -47,24 +42,27 @@ class PoliticianResource(Resource):
     def put(self, politician_id):
         data = request.get_json()
 
-        politician = next((politician for politician in politician_list
-                           if politician_id == politician_id), None)
+        politician = next((politician for politician in politician_list if politician.id == politician_id), None)
 
         if politician is None:
             return {'message': 'politician not found'}, HTTPStatus.NOT_FOUND
 
         politician.name = data['name']
-        politician.position = data['position']
         politician.description = data['description']
-        politician.gender = data['gender']
-        politician.age = data['age']
-        politician.county = data['county']
-        politician.constituency = data['constituency']
-        politician.ward = data['ward']
-        politician.bio_data = data['bio_data']
-        politician.c_vitae = data['c_vitae']
 
         return politician.data, HTTPStatus.OK
+
+    def delete(self, politician_id):
+
+        politician = next((politician for politician in politician_list if politician.id == politician_id), None)
+
+        if politician is None:
+            return {'message': 'politician not found'}, HTTPStatus.NOT_FOUND
+
+        politician_list.remove(politician)
+
+        return {}, HTTPStatus.NO_CONTENT
+
 
 class PoliticianPublishResource(Resource):
 

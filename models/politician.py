0dc8605fa287acb214acd7517f62394719e9b0e6
Brian Koech <brightkoech@gmail.com>
2020-02-28 09:47:50 +0800

    Added more parameters to model

--- models/politician.py
+++ models/politician.py
@@ -3,21 +3,27 @@ politician_list = []
 
 def get_last_id():
     if politician_list:
-        last_politician = last_politician[-1]
+        last_politician = politician_list[-1]
     else:
         return 1
     return last_politician.id + 1
 
 
 class Politician:
-    def __init__(self, name):
+    def __init__(self, name, position, gender, age):
         self.id = get_last_id()
         self.name = name
+        self.position = position
+        self.gender = gender
+        self.age = age
         self.is_publish = False
 
     @property
     def data(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'position': self.position,
+            'gender': self.gender,
+            'age': self.age
         }

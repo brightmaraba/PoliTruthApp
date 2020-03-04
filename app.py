612a83319a6b4ed652588b7c03880ab773202415
Brian Koech <brightkoech@gmail.com>
2020-03-04 15:26:22 +0800

    Added Token Revoke: Test Done - Proceed

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from extensions import db, jwt
 
 from resources.user import UserResource, UserListResource, MeResource
 from resources.politician import PoliticianListResource, PoliticianResource, PoliticianPublishResource
-from resources.token import  TokenResource, RefreshResource
+from resources.token import  TokenResource, RefreshResource, RevokeResource, black_list
 
 
 def create_app():
@@ -22,9 +22,15 @@ def create_app():
 
 
 def register_extensions(app):
+    db.app = app
     db.init_app(app)
     migrate = Migrate(app, db)
     jwt.init_app(app)
+
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        jti = decrypted_token['jti']
+        return jti in black_list
 
 
 def register_resources(app):
@@ -33,11 +39,14 @@ def register_resources(app):
     api.add_resource(UserListResource, '/users')
     api.add_resource(UserResource, '/users/<string:username>')
     api.add_resource(MeResource, '/me')
+
     api.add_resource(PoliticianListResource, '/politicians')
     api.add_resource(PoliticianResource, '/politicians/<int:politician_id>')
     api.add_resource(PoliticianPublishResource, '/politicians/<int:politician_id>/publish')
+
     api.add_resource(TokenResource, '/token')
     api.add_resource(RefreshResource, '/refresh')
+    api.add_resource(RevokeResource, '/revoke')
 
 
 def set_context(app):
--- config.py
+++ config.py
@@ -3,5 +3,10 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ptadbadmin:5114@localhost/ptadb'
     SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+
     SECRET_KEY = 'super-secret-key'
     JWT_ERROR_MESSAGE = 'message'
+
+    JWT_BLACKLIST_ENABLED: True
+    JWT_BLACK_LIST_TOKEN_CHECKS = ['access', 'refresh']
+

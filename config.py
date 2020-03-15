9a86e6c2c2dd92e9309c14ab0f46d3b7256bb895
Brian Koech <brightkoech@gmail.com>
2020-03-15 22:52:03 +0800

    Implemented Rate Limiting: Done

--- config.py
+++ config.py
@@ -2,10 +2,8 @@ import os
 class Config:
     DEBUG = False
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ptadbadmin:5114@localhost:5432/ptadb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_ERROR_MESSAGE_KEY = 'message'
 
     JWT_BLACKLIST_ENABLED = True
@@ -17,3 +15,22 @@ class Config:
     CACHE_DEFAULT_TIMEOUT = 10 * 60
 
     RATE_LIMIT_HEADERS_ENABLED = True
+
+class DevelopmentConfig(Config):
+
+    DEBUG = True
+    SECRET_KEY = 'GimmeHopeJohanna'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ptadbadmin:5114@localhost:5432/ptadb'
+
+class ProductionConfig(Config):
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class StagingConfig(Config):
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+
